794050a5f21b6c917a21e3ce76111d3e2e3b2a8d
Matt Townsen <matthewtownsen@gmail.com>
2017-03-31 16:54:42 -0600

    complete jobs module

--- app/jobs/jobs.js
+++ app/jobs/jobs.js
@@ -11,17 +11,43 @@ angular.module('myApp.jobs', ['ngRoute'])
 
 .controller('JobsCtrl', ['$scope', 'JobFactory', function($scope, JobFactory) {
 
+	$scope.showCreateForm = false;
 	JobFactory.getJobs()
 	 	.success(function(data){
 	 		$scope.jobs = data;
 	 	});
 
-	$scope.addJob = function() {
-		JobFactory.addJob($scope.newJob)
-		 	.success(function(data) {
-
-	 		});
+	$scope.addJob = function(newJob) {
+		$scope.showNewForm = false;
+	    JobFactory.addJob(newJob);
+	    $scope.jobs.push(newJob);
 	};
+
+	$scope.removeJob = function(job) {
+	    var index = $scope.jobs.indexOf(job);
+	    $scope.jobs.splice(index, 1); 
+	    JobFactory.deleteJob(newJob);
+	};
+
+	$scope.saveJob = function(job) {
+		JobFactory.updateJob(job);
+	};
+
+	$scope.showCreateForm = function() {
+		$scope.showCreateForm = true;
+	};
+
+	$scope.dynos = [
+	 { value: 'free', name: 'Free' },
+	 { value: 'medium', name: 'Medim' },
+	 { value: 'large', name: 'Large' }
+    ];
+
+    $scope.frequencys = [
+	 { value: 'daily', name: 'Daily' },
+	 { value: 'weekly', name: 'Weekly' },
+	 { value: 'monthly', name: 'Monthly' }
+    ];
 
 }])
 
@@ -29,6 +55,9 @@ angular.module('myApp.jobs', ['ngRoute'])
 	return {
 		getJobs: function() {
 			return $http.get('/jobs/jobs.json');
+		},
+		updateJob: function(job) {
+
 		},
 		addJob: function(job) {
 
