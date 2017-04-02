10a88e6813abea63aa646ea65361ca0df870aa45
Matt Townsen <matthewtownsen@gmail.com>
2017-04-02 09:37:47 -0600

    remove unnecessary ui-bootstrap component

--- app/app.js
+++ app/app.js
@@ -3,7 +3,6 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'ui.bootstrap',
   'myApp.jobs'
 ])
 .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
